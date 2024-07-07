af2eda48ce380ad93c12c7e2b72677438f964090
yannickhenny <yannick.henny@googlemail.com>
2024-07-07 23:30:09 +0200

    adding basic implementation for token and authorisation as a cassandra user

--- README.md
+++ README.md
@@ -8,7 +8,31 @@ T install the dependencies, switch to the projects root dir ( the one you should
 pip install -r requirements.txt --break-system-packages
 ```
 this should install the required packages.
+
+## Environment variables
+The API needs a set of environment variables to run correctly.
+Generate the environment variables by creating a .env file in the /api folder.
+
+Populate the .env file with the following environment variables:
+```.env
+CASSANDRA_URLS='localhost, 127.0.0.1'
+CASSANDRA_PORT=9042
+CASSANDRA_KEYSPACE='IoTData'
+CASSANDRA_USERNAME='YouCanLeaveThisEmpty'
+CASSANDRA_PASSWORD='YouCanLeaveThisEmpty'
+
+JWT_SECRET='generated jwt secret'
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+API_PORT=8000
+```
+Replace the JWT_SECRET with a secret 32 bit long hash digest, that you generate with openssl:
+
+``sh
+openssl rand -hex 32
+```
 ## Running the api
+Before running the api, one must setup the env variables correctly. 
+See therefore the section 'Environment variables'
 The api uses by standard the uvicorn asgi server.
 Run the api with the following command:
 ```sh
--- api/api_main.py
+++ api/api_main.py
@@ -1,13 +1,76 @@
-from fastapi import FastAPI
-import uvicorn
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security.http import HTTPAuthorizationCredentials, HTTPBearer
+from cassandra import ConsistencyLevel
+from cassandra.cluster import Cluster, Session, ResultSet
+from cassandra.auth import PlainTextAuthProvider
+from cassandra.query import SimpleStatement
+import uvicorn, dotenv, os, jwt, typing
+
+dotenv.load_dotenv()
 app = FastAPI()
+
+cluster_addrs = os.environ.get("CASSANDRA_URLS").split(",")
+JWT_SECRET = os.environ.get("JWT_SECRET")
+CASSANDRA_PORT = os.environ.get("CASSANDRA_PORT")
+security = HTTPBearer
+
+"""
+Returns the JWT subtoken
+"""
+def get_jwt_token_up(user:str, password:str):
+    token = jwt.encode({"username": user, "password": password}, JWT_SECRET, algorithm="HS256")
+    return token
+
+"""
+Returns a dict with the keys "user","password"
+"""
+def get_up_from_jwt_token(jwt_token:str):
+    return jwt.decode(jwt_token, JWT_SECRET, algorithms=["HS256"])
+
+
+
+async def get_token_from_requests(auth: typing.Optional[HTTPAuthorizationCredentials] = Depends(HTTPBearer(auto_error=False))) -> str:
+    if auth is None:
+        raise HTTPException(status_code=401, detail="No token provided")
+    else:
+        token = auth.credentials
+        return token
+
+def get_db_session_atomic(username, password) -> Session:
+    cluster = Cluster(contact_points=cluster_addrs, port = CASSANDRA_PORT)
+    auth_provider = PlainTextAuthProvider(username=username, password=password)
+    cluster.auth_provider = auth_provider
+    return cluster.connect()
 
 @app.get("/")
 def get_root():
     return "Hello to API"
 
+@app.get("/getToken")
+def get_root(username: str, password: str):
+    if (username==None or password == None):
+        raise HTTPException(status_code=400, detail="No username/password provided!")
+    else:
+        try:
+            session = get_db_session_atomic(username,password)
+        except:
+            raise HTTPException(status_code=401, detail="Invalid username/password!")
+        return get_jwt_token_up(username, password)
+
+@app.get("/keyspaces")
+def get_keyspaces(token: str = Depends(get_token_from_requests)):
+    authdict = get_up_from_jwt_token(token)
+    session:Session = get_db_session_atomic(authdict['username'], authdict['password'])
+    query = SimpleStatement("""
+        DESCRIBE KEYSPACES;
+        """, consistency_level=ConsistencyLevel.ONE)
+    
+    query_result:ResultSet = session.execute(query)
+    all_keyspaces = list(query_result)
+    return all_keyspaces
+
 def main():
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("API_PORT")))
 
 if(__name__ == '__main__'):
     main()
